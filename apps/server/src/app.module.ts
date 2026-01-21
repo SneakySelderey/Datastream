95d6d4162f42c8d63cbd3aba6f340a3936e7b1d8
Alexander Ershov <ershovsanek.centr@yandex.ru>
2026-01-21 18:56:08 +0300

    init scanner module and service

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScannerModule } from './modules/scanner/scanner.module';
 
 @Module({
-  imports: [],
+  imports: [ScannerModule],
   controllers: [AppController],
   providers: [AppService],
 })
